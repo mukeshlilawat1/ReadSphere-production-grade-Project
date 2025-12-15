be447ac17ac77c5b3d978e923b533cc53ed5d060
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-12-15 21:55:35 +0530

    Initial commit: ReadSphere project structure and service module

---
+++ ReadSphere_Service/src/main/java/com/example/demo/ReadSphereServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadSphereServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadSphereServiceApplication.class, args);
+	}
+
+}
